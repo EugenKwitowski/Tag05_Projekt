3add7e2700acfbb6160d8010c6312e9cda6571fa
ekwitows <ekwitows.syntax@gmail.com>
2023-01-17 15:04:24 +0100

    Initial commit

---
+++ src/main/kotlin/1_Wiederholung.kt
@@ -0,0 +1,45 @@
+fun main() {
+
+    // TODO a: Befülle die Values
+    val NAME: String = ("Eugen Kwitowski")
+    val FIRST_NAME: String = ("Eugen")
+    val LAST_NAME: String = ("Kwitowski")
+
+    println("Hallo, mein Voller Name ist $NAME. Mein Vorname ist $FIRST_NAME, und Nachname ist $LAST_NAME.")
+
+    // TODO b: Benenne die Values
+    val PHONE: String = "0123 45678910"
+    val COLOR: String = "Blau"
+    val TO_EAT: String = "Hamburger"
+
+    println("Meine Telefonnummer ist $PHONE, meine Lieblingsfarbe ist $COLOR und ich esse am liebsten $TO_EAT.")
+
+    // TODO c: Befülle die Variablen neu
+    var country: String = "Spanien"
+    country = "Deutschland"
+
+    var city: String = "Berlin"
+    city = "da, wo es besser ist"
+
+    println("Zur Zeit lebe ich in $country. Würde gerne leben $city.")
+
+    // TODO d: Befülle die Variable mit einer Eingabe des Users
+
+    var continent: String = readln()
+
+
+    // TODO e: Schlüsselwort var oder val einfügen
+    val TIME: String = "09:00"
+    var placeOfBirth: String = "München"
+    val PET_NAME: String = "Brutus"
+    var currentTVShow: String = "Breaking Bad"
+    var lastVacation: String = "Tokyo"
+
+    println("Es ist gerade $TIME. Ich wurde in $placeOfBirth geboren und habe ein Haustier namens $PET_NAME." +
+            "Meine Lieblinssendung ist $currentTVShow. Mein letzter Urlaub war in $lastVacation.")
+    
+    
+    // TODO: Nutze Println um dem User eine kleine Textbeschreibung auszugeben
+    
+
+}
